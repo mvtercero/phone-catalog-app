a9467f3020ecc0a27d83916a169ea8ad43be1ee6
María Vedia <mvtercero85@gmail.com>
2018-09-15 19:52:40 +0200

    Changed the cors in app.js

--- routes/phones.js
+++ routes/phones.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.json({
-    "phones": [
-      { id: 1, title: "iPhone 7" },
-      { id: 2, title: "Samsung Galaxy" }
-    ]
-  });
+  res.json(
+    {
+      "phones": [
+        { id: 1, title: "iPhone 7" },
+        { id: 2, title: "Samsung Galaxy" }
+      ]
+    }
+  );
 });
 
 module.exports = router;
