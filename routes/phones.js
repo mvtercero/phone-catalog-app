4fbf92f63f10c84556bfd1278666b01b0b2aeb14
María Vedia <mvtercero85@gmail.com>
2018-09-15 20:46:36 +0200

    Modified API for include price, color, description and url image

--- routes/phones.js
+++ routes/phones.js
@@ -6,8 +6,24 @@ router.get('/', function (req, res, next) {
   res.json(
     {
       "phones": [
-        { id: 1, title: "iPhone 7" },
-        { id: 2, title: "Samsung Galaxy" }
+        {
+          id: 1,
+          title: "Apple iPhone XS",
+          price: 44.50 + "$",
+          color: "plata, gris espacial, oro",
+          description: "Sofisticado, de calidad y con gran pantalla de retina",
+          url_image: "https://www.vodafone.es/img/dynamique/LISTADO_FINANCING/77b607e5693c47cc87c9b535a86d3095_158x270_iphoneXs-gold.png"
+        },
+
+        {
+          id: 2,
+          title: "Samsung Galaxy A6 Negro",
+          price: 6 + "$",
+          color: "plata, negro, oro",
+          description: "La diversión sin límites, a tu bolsillo",
+          url_image: "https://www.vodafone.es/img/dynamique/LISTADO_FINANCING/b52fbdb7246f436fb50e7d9c7629d842_158x270-A6.png"
+        },
+
       ]
     }
   );
